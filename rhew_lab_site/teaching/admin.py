72e9f05262990eaee6282cb2b1e7341cd1dcce48
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-14 01:00:40 -0800

    Course admin has been configured with search fields, ordering, list filter and list display.

--- rhew_lab_site/teaching/admin.py
+++ rhew_lab_site/teaching/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+# from django.contrib.admin import AdminSite
 
-# Register your models here.
+from .models import Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = (
+        "course_dept_num",
+        "semester_season",
+        "semester_year",
+        "description",
+    )
+    ordering = ["-semester_year"]
+    list_filter = ["school_year", "course_dept_num"]
+    list_display = (
+        "course_dept_num",
+        "semester_season",
+        "semester_year",
+        "description",
+    )
+
+
+admin.site.register(Course, CourseAdmin)
