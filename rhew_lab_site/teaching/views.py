16a1e6c401c8984aeb46b528e25e25bc96583439
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-14 13:43:12 -0800

    updated index view to include an ordered set of school years to the course_list  template (which is the new template for this view).

--- rhew_lab_site/teaching/views.py
+++ rhew_lab_site/teaching/views.py
@@ -5,4 +5,8 @@ from . import models
 
 def index(request):
     courses = models.Course.objects.all()
-    return render(request, "teaching/teaching.html", {"courses": courses})
+    school_years = [course.school_year for course in courses]
+    school_years = sorted(set(school_years), reverse=True)
+    return render(request,
+                  "teaching/course_list.html",
+                  {"courses": courses, "school_years": school_years, })
