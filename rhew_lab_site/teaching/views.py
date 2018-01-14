9b78b85f295aae770c46c1a723bb5ca1d85cbb01
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-14 11:48:09 -0800

    index view now returns a courses object.

--- rhew_lab_site/teaching/views.py
+++ rhew_lab_site/teaching/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def index(request):
-    pass
+    courses = models.Course.objects.all()
+    return render(request, "teaching/teaching.html", {"courses": courses})
