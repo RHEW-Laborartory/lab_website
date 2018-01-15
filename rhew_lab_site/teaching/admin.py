0f44df20552ad331050b7dfb1eccb90617efc12b
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-14 21:05:00 -0800

    added course_state for search fields, list filter, list display. Added radio button options for course_state

--- rhew_lab_site/teaching/admin.py
+++ rhew_lab_site/teaching/admin.py
@@ -10,15 +10,18 @@ class CourseAdmin(admin.ModelAdmin):
         "semester_season",
         "semester_year",
         "description",
+        "course_state"
     )
     ordering = ["-semester_year"]
-    list_filter = ["school_year", "course_dept_num"]
+    list_filter = ["school_year", "course_dept_num", "course_state"]
     list_display = (
         "course_dept_num",
         "semester_season",
         "semester_year",
         "description",
+        "course_state"
     )
+    radio_fields = {"course_state": admin.VERTICAL}
 
 
 admin.site.register(Course, CourseAdmin)
