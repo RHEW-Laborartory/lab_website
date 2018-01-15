8cbe05d7c56d744dc74e5af7a2b0804020f8fdeb
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-14 21:03:36 -0800

    added verbose name for course_dept_number, added course_state field with choices

--- rhew_lab_site/teaching/models.py
+++ rhew_lab_site/teaching/models.py
@@ -10,9 +10,15 @@ class Course(models.Model):
     semester_season = models.CharField(max_length=255, verbose_name="Semester")
     description = models.CharField(max_length=255)
     course_dept_num = models.CharField(max_length=255,
-                                       default="",
-                                       verbose_name="Course Number"
+                                       verbose_name="Course Number",
                                        )
+
+    STATE_CHOICES = (("Previous Year", "Previous Year"),
+                     ("Current Year", "Current Year"),
+                     ("Next Year", "Next Year"))
+    course_state = models.CharField(
+        max_length=255, choices=STATE_CHOICES, default="previous_year"
+    )
 
     class Meta:
         ordering = ["-semester_year"]
