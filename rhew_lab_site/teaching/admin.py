9ec399811a2dac73daf16b05b27d811abd94fcc7
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-18 23:09:11 -0800

    added course page admin

--- rhew_lab_site/teaching/admin.py
+++ rhew_lab_site/teaching/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 # from django.contrib.admin import AdminSite
 
-from .models import Course
+from .models import Course, CoursePage
 
 
 class CourseAdmin(admin.ModelAdmin):
@@ -10,7 +10,7 @@ class CourseAdmin(admin.ModelAdmin):
         "semester_season",
         "semester_year",
         "description",
-        "course_state"
+        "course_state",
     )
     ordering = ["-semester_year"]
     list_filter = ["school_year", "course_dept_num", "course_state"]
@@ -24,4 +24,25 @@ class CourseAdmin(admin.ModelAdmin):
     radio_fields = {"course_state": admin.VERTICAL}
 
 
+class CoursePageAdmin(admin.ModelAdmin):
+    fields = ('course_dept_num',
+              'course_title',
+              'full_description'
+              )
+    search_fields = (
+        "last_updated",
+        "full_description",
+        "course_dept_num",
+        "course_title",
+    )
+    ordering = ["course_dept_num"]
+    list_filter = ["last_updated"]
+    list_display = [
+        "course_dept_num",
+        "course_title",
+        "last_updated"
+    ]
+
+
 admin.site.register(Course, CourseAdmin)
+admin.site.register(CoursePage, CoursePageAdmin)
