21dd36c7040e0329ba6fd082bdc19dfc6a1ae5df
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-18 23:11:53 -0800

    added course page model

--- rhew_lab_site/teaching/models.py
+++ rhew_lab_site/teaching/models.py
@@ -17,13 +17,37 @@ class Course(models.Model):
                      ("Current Year", "Current Year"),
                      ("Next Year", "Next Year"))
     course_state = models.CharField(
-        max_length=255, choices=STATE_CHOICES, default="previous_year"
+        max_length=255, choices=STATE_CHOICES, default="Previous Year"
     )
 
     class Meta:
         ordering = ["-semester_year"]
 
 
-class CourseDetail(models.Model):
+class CoursePage(models.Model):
     """Model designed for course detail pages."""
-    pass
+    last_updated = models.DateTimeField(auto_now_add=True, null=True)
+    full_description = models.TextField(verbose_name="Description", default="")
+    course_dept_num = models.CharField(
+        max_length=255, verbose_name="Course Number", default=""
+    )
+    course_title = models.CharField(
+        max_length=255, verbose_name="Title", default=""
+    )
+
+    class Meta:
+        ordering = ["course_dept_num"]
+
+    def previous_years(self, course_dept_num):
+        previous_years = []
+        courses = models.Course.objects.all().filter(
+            course_dept_num=course_dept_num,
+        )
+        for course in courses:
+            year = course.semester_year
+            semseter = course.semester_season
+            previous_years.append((year, semseter))
+        return sorted(previous_years)
+
+
+
