1e22dd621a738914a744e2e02ca9947950b1a7ca
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-18 23:11:26 -0800

    index view sends courses based on next year, current year ......

--- rhew_lab_site/teaching/views.py
+++ rhew_lab_site/teaching/views.py
@@ -4,9 +4,25 @@ from . import models
 
 
 def index(request):
-    courses = models.Course.objects.all()
-    school_years = [course.school_year for course in courses]
+    next_years_courses = models.Course.objects.all().filter(
+        course_state="Next Year"
+    )
+    next_school_year = next_years_courses[0].school_year
+    current_years_courses = models.Course.objects.all().filter(
+        course_state="Current Year"
+    )
+    current_school_year = current_years_courses[0].school_year
+    previous_years_courses = models.Course.objects.all().filter(
+        course_state="Previous Year"
+    )
+    school_years = [course.school_year
+                    for course in models.Course.objects.all()]
     school_years = sorted(set(school_years), reverse=True)
     return render(request,
                   "teaching/course_list.html",
-                  {"courses": courses, "school_years": school_years, })
+                  {"next_years_courses": next_years_courses,
+                   "next_school_year": next_school_year,
+                   "current_years_courses": current_years_courses,
+                   "current_school_year": current_school_year,
+                   "previous_years_courses": previous_years_courses,
+                   "school_years": school_years, })
